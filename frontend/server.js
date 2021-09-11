38d1703121e0ed8642140b268c6e604158eba304
NitinMa2 <nitinmathew.official@gmail.com>
2021-09-12 00:52:13 +0800

    fix for relative paths to images

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,10 @@
 const express = require("express"); // express server
+const path = require("path"); // for relative paths
 const port = process.env.PORT || 8080; // use port specified by system or fallback to 8080
 const app = express(); // instantiate server
 
 // Serve all the files in '/dist' directory
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.listen(port, function() {
     console.log("App listening on port " + port);
