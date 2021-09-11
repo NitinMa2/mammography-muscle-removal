2b3e8fbfbad20fad38a60f8443eecc4113ac6690
NitinMa2 <nitinmathew.official@gmail.com>
2021-09-12 00:56:40 +0800

    commenting the images

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,11 @@
 const express = require("express"); // express server
-const path = require("path"); // for relative paths
+// const path = require("path"); // for relative paths
 const port = process.env.PORT || 8080; // use port specified by system or fallback to 8080
 const app = express(); // instantiate server
 
 // Serve all the files in '/dist' directory
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static("dist"));
 
 app.listen(port, function() {
     console.log("App listening on port " + port);
