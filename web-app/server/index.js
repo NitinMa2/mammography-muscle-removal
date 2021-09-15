9e173ef4ea6278484abd5b701d7887baa102036a
NitinMa2 <nitinmathew.official@gmail.com>
2021-09-15 23:34:51 +0800

    updating url

--- web-app/server/index.js
+++ web-app/server/index.js
@@ -25,7 +25,7 @@ app.use("/api/segmentation", segmentation);
 // Handle production
 if (process.env.NODE_ENV === "production") {
     // Static folder
-    app.use(express.static(path.join(__dirname, "/public")));
+    app.use(express.static(path.join(__dirname, "/public/")));
 
     // Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + "public/index.html"));
