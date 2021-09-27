5a6ecbca00bf660f0590810f2cdadd06fc7ff4f5
Usama Qasim Mian <usama1997qasim@gmail.com>
2021-09-27 22:54:39 +0500

    utf-8 decode for ease of use in JS

--- rest-api/main.py
+++ rest-api/main.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
-
+import region_growing
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return {"name": name}
+class Segment(Resource):
+    def get(self, base64):
+        output = region_growing.run_region_growing_on_image(base64)
+        return {"segmented": output}
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Segment, "/segment/<string:base64>")
 if __name__ == "__main__":
     app.run(debug=True)
