cd4b70dfd0b522845c235c8164fdbe12d3dcf87d
Usama Qasim Mian <usama1997qasim@gmail.com>
2021-09-30 09:51:48 +0500

    some validations

--- rest-api/main.py
+++ rest-api/main.py
@@ -16,19 +16,38 @@ import region_growing
 #     app.run(debug=True)
 
 from flask import request, Flask
-from PIL import Image
+from flask_restful import reqparse
+import base64
 
 app = Flask(__name__)
+
+base64_string_post_args = reqparse.RequestParser()
+base64_string_post_args.add_argument("base64 string", type=str, help="Please input string type")
 
 
 @app.route('/segment/<path:base64str>', methods=['POST'])
 def segment(base64str):
     if request.method == 'POST':
-
-        output = region_growing.run_region_growing_on_image(base64str)
-
+        if isBase64(base64str):
+            output = {"segmented Image": region_growing.run_region_growing_on_image(base64str)}
+        else:
+            output = {"Input Error": "Please Input Base64 String Type"}
         return output
     pass
+
+
+def isBase64(sb):
+    try:
+        if isinstance(sb, str):
+            # If there's any unicode here, an exception will be thrown and the function will return false
+            sb_bytes = bytes(sb, 'ascii')
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
 
 
 if __name__ == '__main__':
--- rest-api/test.py
+++ rest-api/test.py
@@ -10,8 +10,12 @@ def img2string(path):
         encoded_string = base64.b64encode(image_file.read())
         image_string = encoded_string.decode('utf-8')
         response = requests.post(BASE + "segment/" + image_string)
-        print(response.text)
+        print(response.json())
 
+
+
+response = requests.post(BASE + "segment/sds")
+print(response.json())
 
 for i in range(1, 10):
     img2string("testing_images/mdb00" + str(i) + ".pgm")
