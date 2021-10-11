028000c588c9e9e770d33a06bd212c4ebf0d9677
NitinMa2 <nitinmathew.official@gmail.com>
2021-10-12 01:57:22 +0800

    removing history

--- web-app/server/index.js
+++ web-app/server/index.js
@@ -14,7 +14,7 @@ app.use(
         extended: true,
     })
 );
-app.use(history()); // allows the client to visit routes directly
+// app.use(history()); // allows the client to visit routes directly
 
 // handling api calls to backend
 const segmentation = require("./routes/api/segmentation");
